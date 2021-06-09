8df23b4519758dca2dfae9021d08a650a9896e03
robxcalib3r <khisarobin@gmail.com>
2021-06-09 21:13:59 +0600

    Version 1.0

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -14,15 +14,21 @@ import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import android.graphics.Bitmap;
 
 import org.opencv.aruco.Aruco;
+import org.opencv.aruco.DetectorParameters;
 import org.opencv.aruco.Dictionary;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfInt;
+
+import static java.lang.Math.sqrt;
+
 
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
@@ -62,16 +68,52 @@ public class YourService extends KiboRpcService {
 
         //Aruco AR Tag reading starts from here
         Mat img = api.getMatNavCam();
-        MatOfInt markerIDs = new MatOfInt();
-        LinkedList<Mat> markerCorners = new LinkedList<>();
+        //Vector<Integer> markerIDs = new Vector<>();
+        Mat markerIDs = new Mat();
+        List<Mat> markerCorners = new ArrayList<>();
         Dictionary dictionary= Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
-        //DetectorParameters param = DetectorParameters.create();
+        DetectorParameters param = DetectorParameters.create();
+        List<Mat> rejectedImgpoints = new ArrayList<>();
+        double[][] camIntrinsics = api.getNavCamIntrinsics();
+
+        //double[] cameraMatrix = Arrays.stream(camIntrinsics).map(r -> r[0]).toArray(double[]::new);
+        //double[] distcoeff = Arrays.stream(camIntrinsics).map(r -> r[1]).toArray(double[]::new);
+        // Needs java 8 for lambda expression
+
+        double[] camMat = camIntrinsics[0];
+        Mat cameraMatrix = new Mat(3, 3, CvType.CV_32F);
+        sendData(MessageType.STRING, "data", String.valueOf(camMat));   //for debugging purpose
+        cameraMatrix.put(0, 0, camMat);
+
+        double[] distDouble = camIntrinsics[1];
+        sendData(MessageType.STRING, "data", String.valueOf(distDouble));   //for debugging purpose
+        Mat distCoeffs = new Mat(1, 5, CvType.CV_32F);
+        distCoeffs.put(0,0, distDouble);
 
 
-        Aruco.detectMarkers(img, dictionary, markerCorners, markerIDs); //, param);
-        double[] data =  markerIDs.get(0, 0);
 
-        //sendData(MessageType.STRING, "data", String.valueOf(data));
+        Aruco.detectMarkers(img, dictionary, markerCorners, markerIDs, param, rejectedImgpoints, cameraMatrix, distCoeffs);
+
+        for (int i = 0; i< markerIDs.rows(); i++) {
+            //for (int j = 0; j< markerIDs.cols(); j++){
+                int id = (int)markerIDs.get(i,0)[0];
+                sendData(MessageType.STRING, "data", String.valueOf(id));    //sending data for each individual marker
+            //}
+        }
+
+
+        Mat rvecs = new Mat();
+        Mat tvecs = new Mat();
+        float markerLen = 0.05f;
+
+        Aruco.estimatePoseSingleMarkers(markerCorners, markerLen, cameraMatrix, distCoeffs, rvecs, tvecs);
+
+        Mat rotMat = new Mat(3,3, CvType.CV_64F);
+
+        Calib3d.Rodrigues(rvecs, rotMat);   // 1x3 rotation vector to 3x3 rotation matrix
+
+
+
 
 
     }
@@ -109,10 +151,9 @@ public class YourService extends KiboRpcService {
 
     private String qrScan(){            //Seperate QR scanning function with redundancy enabled
         final int LOOP_MAX = 3;
-        boolean succeed = false;
         com.google.zxing.Result qrcode = null;
         int loopCounter = 0;
-        while (!succeed || loopCounter < LOOP_MAX){
+        while (qrcode == null || loopCounter < LOOP_MAX){
             Bitmap bm = api.getBitmapNavCam();
             int[] intB1 = new int[bm.getWidth()*bm.getHeight()];
             bm.getPixels(intB1, 0 , bm.getWidth(), 0, 0, bm.getWidth(), bm.getHeight());
@@ -120,9 +161,10 @@ public class YourService extends KiboRpcService {
             BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
             try{
                 qrcode = new MultiFormatReader().decode(bitmap);
-                succeed = true;
             }
-            catch(Exception e){}
+            catch(Exception e){
+                qrcode = null;
+            }
             ++loopCounter;
         }
         return qrcode.getText();
@@ -141,6 +183,34 @@ public class YourService extends KiboRpcService {
 
     private double S2D(String s){
         return Double.parseDouble(s);
+    }
+
+    private void getQuaternion(Mat R, double Q[]){
+        double trace = R.get(0,0)[0] + R.get(1,1)[0] + R.get(2,2)[0];
+
+        if (trace > 0.0){
+            double s = sqrt(trace + 1.0);
+            Q[3] = (s * 0.5);
+            s = 0.5 / s;
+            Q[0] = ((R.get(2,1)[0] - R.get(1,2)[0]) * s);
+            Q[1] = ((R.get(0,2)[0] - R.get(2,0)[0]) * s);
+            Q[2] = ((R.get(1,0)[0] - R.get(0,1)[0]) * s);
+        }
+
+        else{
+            int i = R.get(0,0)[0] < R.get(1,1)[0] ? (R.get(1,1)[0] < R.get(2,2)[0] ? 2:1) : (R.get(0,0)[0] < R.get(2,2)[0] ? 2 : 0);
+            int j = (i + 1) % 3;
+            int k = (i + 2) % 3;
+
+            double s = sqrt(R.get(i, i)[0] - R.get(j,j)[0] - R.get(k,k)[0] + 1.0);
+            Q[i] = s * 0.5;
+            s = 0.5 / s;
+
+            Q[3] = (R.get(k,j)[0] - R.get(j,k)[0]) * s;
+            Q[j] = (R.get(j,i)[0] + R.get(i,j)[0]) * s;
+            Q[k] = (R.get(k,i)[0] + R.get(i,k)[0]) * s;
+        }
+        
     }
 
 }
